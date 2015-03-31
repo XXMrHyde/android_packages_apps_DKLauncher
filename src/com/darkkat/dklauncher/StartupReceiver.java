63d2e15fd45228a150e982c9b1f010b727d71a58
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-04-01 01:48:24 +0200

    DKLauncher part 2

---
+++ src/com/darkkat/dklauncher/StartupReceiver.java
@@ -0,0 +1,15 @@
+package com.darkkat.dklauncher;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    static final String SYSTEM_READY = "com.darkkat.dklauncher.SYSTEM_READY";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.sendStickyBroadcast(new Intent(SYSTEM_READY));
+    }
+}
